da6f2b6501d697bf935916ac0114427344f21326
Your Name <your.email@example.com>
2025-07-31 17:05:22 +0530

    new vec

--- R_basics/bar.R
+++
@@ -1,7 +0,0 @@
-
-fruits <- c("Apple", "Banana", "Orange", "Mango")
-counts <- c(10, 15, 8, 12)
-
-
-barplot(counts, names.arg=fruits, col="skyblue", main="Fruit Count", ylab="Count")
-
---
+++ vectors/distinctvector.r
@@ -0,0 +1,4 @@
+v<-c(1,3,4,55,6,5,5,4,4,2,3)
+print(v)
+c<-unique(v)
+print(c)
